a950dbcf91d15e956e533b193aad587c7a646245
kombou dilane <68383483+dilane3@users.noreply.github.com>
2023-04-27 19:27:41 +0000

    Add /users endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,18 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (_req, res) => {
   res.send('Hello World! How are you');
 });
+
+app.get("/users", (_req, res) => {
+  res.json([
+    { id: 1, name: "John Doe" },
+    { id: 2, name: "Jane Doe Julie" },
+  ])
+})
 
 export default app;
